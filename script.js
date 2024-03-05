27276edb5c3d1afeaa98b31b651ae8c06ef53ee6
Ashtacon <astacialvndr@gmail.com>
2024-03-05 22:00:45 +0700

    feat: add job

--- script.js
+++ script.js
@@ -11,42 +11,36 @@ typewriter
   .deleteChars(15)
   .typeString("Front-end WebDev.")
   .pauseFor(500)
-  .deleteChars(23)
+  .deleteChars(17)
   .typeString("Full Stack WebDev.")
+  .pauseFor(500)
+  .deleteChars(19)
+  .typeString("Cosplayer")
   .pauseFor(1000)
   .start();
 
-// Mendapatkan elemen dengan ID "tanggal"
 const tanggalElement = document.getElementById("tanggal");
 
-// Fungsi untuk memperbarui waktu
 function updateClock() {
   const currentDate = new Date();
   const tanggal = currentDate.getDate();
-  const bulan = currentDate.getMonth() + 1; // Ingat bahwa bulan dimulai dari 0
+  const bulan = currentDate.getMonth() + 1;
   const tahun = currentDate.getFullYear();
   const jam = currentDate.getHours();
-  //   const menit = currentDate.getMinutes();
-  //   const detik = currentDate.getSeconds();
   const menit = ("0" + currentDate.getMinutes()).slice(-2);
   const detik = ("0" + currentDate.getSeconds()).slice(-2);
   const tanggalFormatted = `${tanggal}-${bulan}-${tahun}`;
   const jamFormatted = `${jam}:${menit}:${detik}`;
 
-  // Mendapatkan zona waktu lokal pengguna
   const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-  // Mendapatkan waktu lokal sesuai zona waktu pengguna
   const waktuLokal = currentDate.toLocaleString("id-ID", {
     timeZone: userTimeZone,
   });
 
-  // Menambahkan teks tanggal dan jam ke dalam elemen dengan ID "tanggal"
   tanggalElement.textContent = `Time: ${jamFormatted} | ${tanggalFormatted}`;
 }
 
-// Memanggil fungsi pertama kali
 updateClock();
 
-// Mengatur interval pembaruan waktu setiap detik (1000 milidetik)
 setInterval(updateClock, 1000);
