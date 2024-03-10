7a86799d4eb47c2d03d914530c62adfc3cae881e
Ashtacon <astacialvndr@gmail.com>
2024-03-10 20:18:11 +0700

    feat: add PWA

--- script.js
+++ script.js
@@ -54,3 +54,11 @@ const toggleSound = () => {
     audio.pause();
   }
 };
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
